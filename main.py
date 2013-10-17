306895c3d58153c4834942311a801bae672911d3
ekianjo <ekianjo@gmail.com>
2013-10-17 18:38:44 +0900

    Update main.py
    a few updates...

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ import PyZenity
 #zenity dialog
 
 a=PyZenity.InfoMessage("This Application will let you download Google Books available in browser mode in JPG format locally for offline reading", timeout=10)
+a=PyZenity.InfoMessage("Note that you need Java to be installed for this application to run", timeout=3)
 
-b=PyZenity.GetText("Please paste the path of the Google book to download","paste your url here")
+b=PyZenity.GetText("Please paste the path of the Google book to download","URL here")
 
 
 #zenity ask for folder where to save
 #make try except to catch errors of permissions
 #check internet connection too
+
+if b!="":
+  cmd = 'java -jar GoogleBooksDownloader.jar {0} "BOOKS"'.format(b)
+  #cmd = 'zenity --progress --text="Backing Up Games Saves..." --auto-close'
+  proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
 
 #main command
 #java -jar GoogleBooksDownloader.jar "http://books.google.com/...id=sdf?asdfsdf..."  "/location/where/you/want/to/save/your/file/"
