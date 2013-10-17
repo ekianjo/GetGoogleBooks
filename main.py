451dea4df960ed26c64aff4334bcc9d2b3adc3c0
ekianjo <ekianjo@gmail.com>
2013-10-17 18:32:09 +0900

    Update main.py
    first commands.

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import subprocess, os
 import PyZenity
 
 #that's a one hour project probably :)
-#need the jar file
+#need the jar file from https://github.com/bhattumang7/Google-Books-Downloader
 
 #zenity dialog
+
+a=PyZenity.InfoMessage("This Application will let you download Google Books available in browser mode in JPG format locally for offline reading", timeout=10)
+
+b=PyZenity.GetText("Please paste the path of the Google book to download","paste your url here")
+
+
 #zenity ask for folder where to save
 #make try except to catch errors of permissions
 #check internet connection too
