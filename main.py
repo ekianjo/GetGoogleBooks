ebf43c36deef3c369216f0691e9ff255ccfa8c72
ekianjo <ekianjo@gmail.com>
2013-10-17 17:45:29 +0900

    Create main.py
    flow defined

---
+++ main.py
@@ -0,0 +1,19 @@
+import subprocess, os
+import PyZenity
+
+#that's a one hour project probably :)
+#need the jar file
+
+#zenity dialog
+#zenity ask for folder where to save
+#make try except to catch errors of permissions
+#check internet connection too
+
+#main command
+#java -jar GoogleBooksDownloader.jar "http://books.google.com/...id=sdf?asdfsdf..."  "/location/where/you/want/to/save/your/file/"
+
+#check if progress bar if possible
+#run under subprocess
+
+#check existence of files in said folder
+#if files are there, confirm download is done
